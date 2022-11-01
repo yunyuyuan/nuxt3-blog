614f324aea2951c6b78c51dfa8dc9ebb71fa43c1
yunyuyuan <me@halberd.cn>
2022-11-01 22:24:09 +0800

    chore: update README

--- config.ts
+++ config.ts
@@ -1,13 +1,13 @@
 export default {
   title: "yun yu yuan", // 网站标题
   nickName: "云与原", // 昵称
-  githubName: "yunyuyuan", // github账户名
+  githubName: "yunyuyuan", // 必须修改，github账户名
   githubRepo: "nuxt3-blog", // 勿修改
   githubBranch: "master", // 勿修改
   domain: "https://blog.yunyuyuan.net", // rss域名
-  SEO_title: " - yunyuyuan blog",
-  SEO_description: "云与原的博客，书写值得书写的东西",
-  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客",
+  SEO_title: " - yunyuyuan blog", // 搜索引擎显示的标题
+  SEO_description: "云与原的博客，书写值得书写的东西", // description meta header
+  SEO_keywords: "yunyuyuan,yunyuyuan's blog,云与原,云与原的博客", // keywords meta header
 
   themeColor: "#2aa0bb", // 主题色
   CommentRepoId: "R_kgDOGRGGfg", // https://giscus.app/zh-CN
