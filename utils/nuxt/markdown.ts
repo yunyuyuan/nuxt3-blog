d3eac57ea89783c06012511ef50194d359228f2c
yunyuyuane <me@yunyuyuan.net>
2025-03-09 23:29:35 +0800

    refactor(ui): improve code block styling and structure

--- utils/nuxt/markdown.ts
+++ utils/nuxt/markdown.ts
@@ -69,7 +69,7 @@ export async function parseMarkdown(text: string) {
           // hydration，先escape，留在afterInsertHtml里parse
           text = escaped ? text : escapeHtml(text);
         }
-        return `<pre><div><small></small></div><code class="language-${lang} ${isPrerender ? "hljs" : ""}">${text}</code></pre>`;
+        return `<pre data-lang="${lang}"><code class="language-${lang} ${isPrerender ? "hljs" : ""}">${text}</code></pre>`;
       }
     },
     extensions: [
@@ -396,12 +396,7 @@ export async function afterInsertHtml(mdEl: HTMLElement, forEdit = false) {
   await nextTick(async () => {
     // hljs
     mdEl.querySelectorAll<HTMLElement>("pre>code").forEach(async (el) => {
-      const lang = el.parentElement!.querySelector<HTMLElement>(":scope > div > small")!;
-      const language = el.className.replace(/^.*?language-(\S*).*?$/, (_, lang) => {
-        return lang;
-      });
       const hljs = initHljs((await import("highlight.js")).default);
-      lang.innerText = language ? (hljs.getLanguage(language) || { name: language }).name! : " ";
       if (!el.classList.contains("hljs")) {
         hljs.highlightElement(el);
       }
@@ -443,7 +438,7 @@ export async function afterInsertHtml(mdEl: HTMLElement, forEdit = false) {
           vm.unmount();
         });
       });
-    // copy button and theme button in <pre>
+    // copy button in <pre>
     mdEl.querySelectorAll<HTMLPreElement>("pre:not([data-processed])").forEach(async (el) => {
       el.dataset.processed = "true";
       const copyBtn = createSvgIcon(Clipboard);
@@ -462,7 +457,7 @@ export async function afterInsertHtml(mdEl: HTMLElement, forEdit = false) {
       destroyFns.push(() => {
         clipboard.destroy();
       });
-      el.children[0].appendChild(copyBtn);
+      el.appendChild(copyBtn);
     });
     // target=_blank link
     mdEl.querySelectorAll<HTMLLinkElement>("a[target=_blank]:not([data-processed])").forEach((el) => {
